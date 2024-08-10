df8b762847d8759d9502b552e1b995f8cfb25a25
Yehonatan Elentok <elyoni@gmail.com>
2024-08-10 15:05:00 +0300

    [0.1.1] Show error if already in split mode, linting fixes

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ import sys
 from src.xrandr import XRandr
 
 def split_primary():
-    ratio = []
-    for value in sys.argv[1:]:
-        ratio.append(int(value))
-    print(ratio)
+    if len(sys.argv[1:]) > 1:
+        ratio = []
+        for value in sys.argv[1:]:
+            ratio.append(int(value))
+    else:
+        ratio = [70, 30]
     xrandr = XRandr()
     xrandr.split_primary_monitor(ratio)
 
